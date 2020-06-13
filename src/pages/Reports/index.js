ad208ab9629976aa28f99444b15b312d006519d8
croschel <croschel000@gmail.com>
2020-06-13 20:45:11 -0300

    Reports Layouts

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
+import api from '~/services/api';
 
 function Reports() {
-  return <div />;
+  const [mesReferencia, setMesReferencia] = useState('');
+  const [treatmentsYears, setTreatmentsYears] = useState([]);
+  const [anoReferencia, setAnoReferencia] = useState('');
+
+  useEffect(() => {
+    async function loadYears() {
+      const response = await api.get('treatments');
+      const treatmentList = response.data;
+      const years = treatmentList.map(treatment => treatment.ano_referencia);
+
+      setTreatmentsYears(Array.from(new Set(years)));
+    }
+    loadYears();
+  }, [])
+  console.tron.log(mesReferencia, anoReferencia)
+  return (
+    <Container>
+      <h2>Relatórios Mensais</h2>
+      <div>
+        <select id="mes" name="mes_referencia" value={mesReferencia} onChange={(e) => setMesReferencia(e.target.value)}>
+          <option value="" selected="selected disabled hidden" >Selecione</option>
+          <option value="janeiro">Janeiro</option>
+          <option value="fevereiro">Fevereiro</option>
+          <option value="marco">Março</option>
+          <option value="abril">Abril</option>
+          <option value="maio">Maio</option>
+          <option value="junho">Junho</option>
+          <option value="julho">Julho</option>
+          <option value="agosto">Agosto</option>
+          <option value="setembro">Setembro</option>
+          <option value="outubro">Outubro</option>
+          <option value="novembro">Novembro</option>
+          <option value="dezembro">Dezembro</option>
+        </select>
+        <select id="ano" name="mes_referencia" value={anoReferencia} onChange={(e) => setAnoReferencia(e.target.value)}>
+          <option value="" selected="selected disabled hidden" >Selecione</option>
+          {treatmentsYears.map(year => (
+            <option value={year}>{year}</option>
+          ))}
+        </select>
+      </div>
+      <button id="first-button" onClick={() => { }}>Tratamento dos militares Reintegrados por Decisão judicial</button>
+      <button id="second-button" onClick={() => { }}>Ficha de informações de militares em situação especial</button>
+    </Container>
+  );
 }
 
 export default Reports;
---
+++ src/pages/Reports/styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+
+  h2{
+    margin: 30px auto;
+    text-align: center;
+    color: #fff;
+    font-size: 30px;
+    text-shadow: 0px 0px 11px rgba(0,0,0,0.8);
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    width: 50%;
+    margin-top: 50px;
+    margin-bottom: 80px;
+
+    select {
+      height: 50px;
+      font-size: 18px;
+      border-radius: 12px;
+      background: #fff;
+    }
+
+    #mes {
+      width: 70%;
+      margin-right: 1%;
+    }
+    #ano{
+      width: 29%;
+    }
+  }
+
+  button{
+    padding: 20px;
+    font-size: 24px;
+    width: 55%;
+    border: none;
+    border-radius: 22px;
+    margin-bottom: 40px;
+    color: #fff;
+    font-weight: bold;
+
+    
+  }
+  #first-button{
+    background: #CA1A1A;
+    &:hover{
+      background: ${darken(0.15, '#CA1A1A')};
+      transition-duration: 1s;
+
+    }
+  }
+  #second-button{
+    background: #1A21CA;
+    
+    &:hover{
+      background: ${darken(0.15, '#1A21CA')};
+      transition-duration: 1s;
+    }
+  }
+`;
